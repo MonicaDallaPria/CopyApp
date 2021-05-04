750ff598da09e37e19cc9edc59922209694b71fb
mon.hailwood <mon.hailwood@gmail.com>
2021-05-04 10:23:50 +0100

    working app with separate assemblies

--- CopyDirectory/CopyTo.cs
+++ CopyDirectory/CopyTo.cs
@@ -1,35 +1,14 @@
-﻿using System.IO;
+﻿using LogicLibrary;
+using System.IO;
 
 namespace CopyDirectory
 {
-
     public class CopyTo
     {
         public void Copy(string sourceDirectory, string targetDirectory, string fileName)
         {
-
-            string sourcePath = sourceDirectory;
-            string targetPath = targetDirectory;
-
-            string sourceFile = Path.Combine(sourcePath, fileName);
-            string destFile = Path.Combine(targetPath, fileName);
-
-            Directory.CreateDirectory(targetPath);
-
-            if (Directory.Exists(sourcePath))
-            {
-                string[] files = Directory.GetFiles(sourcePath);
-
-                // Copy the files and overwrite destination files if they already exist.
-                foreach (string s in files)
-                {
-                    // Use static Path methods to extract only the file name from the path.
-                    fileName = Path.GetFileName(s);
-                    destFile = Path.Combine(targetPath, fileName);
-                    File.Copy(s, destFile, true);
-                    System.Windows.Forms.MessageBox.Show("Files loaded: " + s.ToString() + " from " + sourceFile + " to " + targetPath, "Message");
-                }
-            }
+            var copyLogic = new CopyLogic();
+            copyLogic.Copy(sourceDirectory, targetDirectory, fileName);
 
         }
 
---
+++ LogicLibrary/CopyLogic.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Windows.Forms;
+
+namespace LogicLibrary
+{
+    public class CopyLogic
+    {
+        public void Copy(string sourceDirectory, string targetDirectory, string fileName)
+        {
+
+            string sourcePath = sourceDirectory;
+            string targetPath = targetDirectory;
+            string destFile = System.IO.Path.Combine(targetPath, fileName);
+
+            string sourceFile = System.IO.Path.Combine(sourcePath, fileName);
+            Directory.CreateDirectory(targetPath);
+
+            if (Directory.Exists(sourcePath))
+            {
+                string[] files = Directory.GetFiles(sourcePath);
+
+                // Copy the files and overwrite destination files if they already exist.
+                foreach (string s in files)
+                {
+                    // Use static Path methods to extract only the file name from the path.
+                    fileName = Path.GetFileName(s);
+                    destFile = Path.Combine(targetPath, fileName);
+                    File.Copy(s, destFile, true);
+                    MessageBox.Show("Files loaded: " + s.ToString() + " from " + sourceFile + " to " + targetPath, "Message");
+                }
+            }
+        }
+    }
+}
